9a09102e12d4ab65e9e613c89dde13bc0efb56dc
IsmailKonderev6030 <73698568+IsmailKonderev6030@users.noreply.github.com>
2022-04-22 07:18:31 +0300

    Add files via upload

---
+++ build-untitled-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/switch_ru.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>HotKeys</name>
+    <message>
+        <location filename="hotkeys.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Диалоговое окно</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.ui" line="79"/>
+        <location filename="hotkeys.ui" line="92"/>
+        <location filename="hotkeys.ui" line="105"/>
+        <location filename="hotkeys.ui" line="118"/>
+        <source>CTRL +</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="12"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="13"/>
+        <location filename="hotkeys.cpp" line="20"/>
+        <source>Exit</source>
+        <translation>Выйти</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="17"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="18"/>
+        <source>Save file</source>
+        <translation>Сохрнаить файл</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="19"/>
+        <source>New file</source>
+        <translation>Создать файл</translation>
+    </message>
+    <message>
+        <location filename="hotkeys.cpp" line="21"/>
+        <source>Enter only one symbol in ENG</source>
+        <translation>Введите букву строго на англ</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="21"/>
+        <source>Text editer</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="23"/>
+        <source>Save file</source>
+        <translation>Сохранить файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="24"/>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="25"/>
+        <source>Hotkeys</source>
+        <translation>Горячие клавиши</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="26"/>
+        <source>Only read</source>
+        <translation>Только для чтения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="27"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="39"/>
+        <source>Info</source>
+        <translation>Инфо</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="57"/>
+        <source>Select file txt</source>
+        <translation>Выберите файл txt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="70"/>
+        <source>Save file txt</source>
+        <translation>Сохраните файл txt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="141"/>
+        <source>Create file txt</source>
+        <translation>Создайте файл txt</translation>
+    </message>
+</context>
+</TS>
---
+++ build-untitled-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,109 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPlainTextEdit *plainTextEdit;
+    QPushButton *pushButton_3;
+    QCheckBox *checkBox;
+    QComboBox *comboBox;
+    QLabel *label;
+    QPushButton *pushButton_4;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(647, 396);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(170, 320, 131, 31));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(320, 320, 131, 31));
+        plainTextEdit = new QPlainTextEdit(centralwidget);
+        plainTextEdit->setObjectName(QString::fromUtf8("plainTextEdit"));
+        plainTextEdit->setGeometry(QRect(10, 0, 621, 251));
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(530, 260, 111, 21));
+        checkBox = new QCheckBox(centralwidget);
+        checkBox->setObjectName(QString::fromUtf8("checkBox"));
+        checkBox->setGeometry(QRect(20, 330, 131, 18));
+        comboBox = new QComboBox(centralwidget);
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(570, 340, 71, 22));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(570, 320, 71, 20));
+        QFont font;
+        font.setPointSize(10);
+        label->setFont(font);
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setGeometry(QRect(530, 290, 111, 21));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 647, 17));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Text Editer", nullptr));
+        centralwidget->  setWindowTitle(QCoreApplication::translate("MainWindow","Text editer", nullptr));
+        pushButton   ->  setText(QCoreApplication::translate("MainWindow","Open file", nullptr));
+        pushButton_2 ->  setText(QCoreApplication::translate("MainWindow","Save file", nullptr));
+        pushButton_3 ->  setText(QCoreApplication::translate("MainWindow","Information", nullptr));
+        pushButton_4 ->  setText(QCoreApplication::translate("MainWindow","Hotkeys", nullptr));
+        checkBox     ->  setText(QCoreApplication::translate("MainWindow","Only read", nullptr));
+        label        ->  setText(QCoreApplication::translate("MainWindow","Language", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
